52f9edfac10e815f90ff166652d14ccb418b6ab2
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-10-01 01:39:02 +0530

    - connected sql to gend

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const mocha = require('mocha');
+const expect = chai.expect
+
+mocha.describe("validator isNumValid()", () => {
+    it("will test", ()=> {
+        // expect(validator.isNumValid(39)).to.be.true
+    })
+})
