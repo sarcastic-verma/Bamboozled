f8643012fdaebb0f248455e0b6001c0d793cd62e
Rithik Bhandari <rithikbhandari895@gmail.com>
2020-12-06 22:41:37 +0530

    ADDED /CREATE

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,11 +1,15 @@
-import express,{Router} from "express";
+import express, {Router} from "express";
 import {FieldInfo, MysqlError} from "mysql";
 import {connectionOptions} from "../app";
 
 const router = Router();
 router.get('/create', (req: express.Request, res: express.Response) => {
     connectionOptions.query(`
-    Select * from users
+    CREATE TABLE IF NOT EXISTS USERS (
+        NAME VARCHAR(30),
+        ADDRESS VARCHAR(30),
+        EMAIL VARCHAR(20) PRIMARY KEY
+    );
     `, (err: MysqlError, rows, fields: FieldInfo) => {
         if (!err) res.send(rows);
         else res.send(err);
