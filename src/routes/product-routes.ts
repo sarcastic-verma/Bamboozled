52afbce7dc48f3ae24cf53d8bf2aacca3448deec
Rithik Bhandari <rithikbhandari895@gmail.com>
2020-12-12 23:55:24 +0530

    - added /create in product-routes.ts - added boiler plate for all routes in product-routes.ts

--- src/routes/product-routes.ts
+++ src/routes/product-routes.ts
@@ -1,18 +1,23 @@
-import express,{Router} from "express";
+import express, {Router} from "express";
 import {FieldInfo, MysqlError} from "mysql";
 import {connectionOptions} from "../app";
 
 const router = Router();
-router.get('/add', (req: express.Request, res: express.Response) => {
+router.get('/create', (req: express.Request, res: express.Response) => {
     connectionOptions.query(`
-    Select * from PRODUCT
+    CREATE TABLE IF NOT EXISTS PRODUCT (   
+        ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,     
+        NAME VARCHAR(50),
+        AMOUNT INT,
+        IMAGE VARCHAR(400)
+    );
     `, (err: MysqlError, rows, fields: FieldInfo) => {
         if (!err) res.send(rows);
         else res.send(err);
     });
 });
 
-router.get('/drop',(req: express.Request, res: express.Response) => {
+router.get('/drop', (req: express.Request, res: express.Response) => {
     connectionOptions.query(`                    
     Drop table PRODUCT;
     `, (err: MysqlError, rows, fields: FieldInfo) => {
@@ -21,7 +26,7 @@ router.get('/drop',(req: express.Request, res: express.Response) => {
     });
 });
 
-router.get('/describe',(req: express.Request, res: express.Response) => {
+router.get('/describe', (req: express.Request, res: express.Response) => {
     connectionOptions.query(`                    
     Describe PRODUCT;
     `, (err: MysqlError, rows, fields: FieldInfo) => {
@@ -29,11 +34,65 @@ router.get('/describe',(req: express.Request, res: express.Response) => {
         else res.send(err);
     });
 });
+
 // GET-ROUTE : fetch all products
+router.get('/', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+                        
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
+
 // GET-ROUTE : search by query
+router.get('/search/:query', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+                        
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
+
 // GET-ROUTE : fetch product by id
+router.get('/:id', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+                        
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
+
 // PATCH-ROUTE : update product by id
+router.patch('/patch/:id', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+                        
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
+
 // POST-ROUTE : add product
+router.post('/add', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+                        
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
+
 // DEL-ROUTE : delete a product
+router.get('/delete/:id', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+                        
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
 
 export default router;
