8f7e5ac39a91a397d15ee8ab1738eb2e9d3578fa
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-12-13 00:02:26 +0530

    - updated transaction-routes

--- src/routes/transaction-routes.ts
+++ src/routes/transaction-routes.ts
@@ -1,37 +1,79 @@
-import express,{Router} from "express";
+import express, {Router} from "express";
 import {FieldInfo, MysqlError} from "mysql";
 import {connectionOptions} from "../app";
 
 const router = Router();
-router.get('/get', (req: express.Request, res: express.Response) => {
+
+router.get('/create', (req: express.Request, res: express.Response) => {
     connectionOptions.query(`
-    Select * from TRANSACTION
+    CREATE TABLE IF NOT EXISTS USER_TRANSACTION (   
+        ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,     
+        ORDER_ID INT,
+        STATUS VARCHAR(100),
+        TRANSACTION_ID VARCHAR(400),
+        FOREIGN KEY (ORDER_ID) REFERENCES USER_ORDER(ID)
+    );
     `, (err: MysqlError, rows, fields: FieldInfo) => {
         if (!err) res.send(rows);
         else res.send(err);
     });
 });
 
-router.get('/drop',(req: express.Request, res: express.Response) => {
-    connectionOptions.query(`                    
-    Drop table TRANSACTION;
+router.get('/', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+    Select * from USER_TRANSACTION;
     `, (err: MysqlError, rows, fields: FieldInfo) => {
         if (!err) res.send(rows);
         else res.send(err);
     });
 });
 
-router.get('/describe',(req: express.Request, res: express.Response) => {
+router.get('/drop', (req: express.Request, res: express.Response) => {
     connectionOptions.query(`                    
-    Describe TRANSACTION;
+    Drop table USER_TRANSACTION;
     `, (err: MysqlError, rows, fields: FieldInfo) => {
         if (!err) res.send(rows);
         else res.send(err);
     });
 });
-// GET-ROUTE : fetch transactions by userId
-// POST-ROUTE : add transaction to userId
-// GET-ROUTE : fetch transaction by id
+
+router.get('/describe', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`                    
+    Describe USER_TRANSACTION;
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
+
+router.get('/get/user/:userId', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+                        
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
+
+
+router.get('/:id', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+                        
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
+
+router.post('/post', (req: express.Request, res: express.Response) => {
+    connectionOptions.query(`
+                        
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
+});
+
 
 
 export default router;
