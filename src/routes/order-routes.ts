93a87a89eb16e9dfa18789658eba42ca6d9394ce
Rithik Bhandari <rithikbhandari895@gmail.com>
2020-12-14 22:57:37 +0530

    completed order-routes.ts

--- src/routes/order-routes.ts
+++ src/routes/order-routes.ts
@@ -10,7 +10,7 @@ router.get('/create', (req: express.Request, res: express.Response) => {
         ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,     
         STATUS VARCHAR(50),
         AMOUNT INT,
-        ORDER_PLACED_DATE DATE,
+        ORDER_PLACED_DATE VARCHAR(50),
         USER_ID INT,
         FOREIGN KEY (USER_ID) REFERENCES USER(ID) 
     );
@@ -57,40 +57,39 @@ router.get('/:id', (req: express.Request, res: express.Response) => {
 });
 
 router.post('/post/order/:userId', (req: express.Request, res: express.Response) => {
-        // add query here
-
-        //     connectionOptions.query(`
-        // Select * from ORDER where id = ${req.params.id};
-        // `, (err: MysqlError, rows, fields: FieldInfo) => {
-        //         if (!err) res.send(rows);
-        //         else res.send(err);
-        //     });
+        connectionOptions.query(`
+    Select * from USER_ORDER where id = ${req.params.id};
+    INSERT INTO USER_ORDER (STATUS,AMOUNT,ORDER_PLACED_DATE,USER_ID)
+    VALUES ("active","${req.body.amount}","${req.body.date}","${req.body.userId}");
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+            if (!err) res.send(rows);
+            else res.send(err);
+        });
     }
 );
 
 
 router.get('/get/user/:userId', (req: express.Request, res: express.Response) => {
-        // add query here
-
-        //     connectionOptions.query(`
-        // Select * from ORDER where id = ${req.params.id};
-        // `, (err: MysqlError, rows, fields: FieldInfo) => {
-        //         if (!err) res.send(rows);
-        //         else res.send(err);
-        //     });
+        connectionOptions.query(`
+    Select * from USER_ORDER where USER_ID = ${req.params.userId};
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+            if (!err) res.send(rows);
+            else res.send(err);
+        });
     }
 );
 
 // PATCH-ROUTE : change order status - { cancel, active, delivered }
 router.patch('/patch/:id', (req: express.Request, res: express.Response) => {
-    // add query here
-
-    //     connectionOptions.query(`
-    // Select * from ORDER where id = ${req.params.id};
-    // `, (err: MysqlError, rows, fields: FieldInfo) => {
-    //         if (!err) res.send(rows);
-    //         else res.send(err);
-    //     });
+    connectionOptions.query(`
+    UPDATE USER_ORDER
+    SET
+    ${req.body.status == null ? "" : `STATUS="${req.body.status}"`}
+    WHERE ID=${req.params.id};
+    `, (err: MysqlError, rows, fields: FieldInfo) => {
+        if (!err) res.send(rows);
+        else res.send(err);
+    });
 });
 
 export default router;
